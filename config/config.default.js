e2c55d487090cd692d62e214980335fcac68327c
AllenChinese <13402202685@163.com>
2020-04-15 23:47:52 +0800

    feat: Setting request domain white list

--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,10 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  config.security = {
+    domainWhiteList: [ 'http://localhost:8081' ],
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
