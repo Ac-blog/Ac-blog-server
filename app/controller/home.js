0941ecd4a5d457d6c941e197eb1096aac4160efe
AllenChinese <13402202685@163.com>
2020-04-16 10:22:56 +0800

    feat: Add article list api

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,45 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    ctx.body = {
+      results: {
+        items: [
+          {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-04',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1517 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-01',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1519 弄',
+            zip: 200333,
+          },
+          {
+            date: '2016-05-03',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1516 弄',
+            zip: 200333,
+          },
+        ],
+      },
+      success: true,
+    };
   }
 }
 
